802d8f36cb08612dc28f7831ec5047b511711b41
satiparpar <mohammad.parpar.99@gmail.com>
2024-12-24 20:04:23 +0330

    update[ routes ]: remove useless comment

--- src/services/wallet/wallet.routes.ts
+++ src/services/wallet/wallet.routes.ts
@@ -58,7 +58,6 @@ export namespace WalletRoutes {
         req: Request<{}, {}, AddApiKeyRequest>,
         res: Response<void, {}>,
       ) => {
-        // validateAddWalletRequest(req.body);
         res.status(200).json(await addApiKey(req.body));
       },
     ),
